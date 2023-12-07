d168e2476152039321ccd460f3822be59fc80c4f
Nick Hamblet <nick.hamblet@gmail.com>
2023-12-06 22:07:52 -0800

    Day 7 Part 2

--- day07/part2/helpers/helpers_test.go
+++ day07/part2/helpers/helpers_test.go
@@ -1,4 +1,21 @@
 package helpers
 
-// "day07/part1/helpers"
-// "testing"
+import (
+	"testing"
+)
+
+func TestHandType(t *testing.T) {
+	var act, exp HandType
+	act, exp = GetHandType([]Card{Ten, Five, Five, Jack, Five}), FourOfAKind
+	if act != exp {
+		t.Errorf("Expected %v, got %v", exp, act)
+	}
+	act, exp = GetHandType([]Card{King, Ten, Jack, Jack, Ten}), FourOfAKind
+	if act != exp {
+		t.Errorf("Expected %v, got %v", exp, act)
+	}
+	act, exp = GetHandType([]Card{Queen, Queen, Queen, Jack, Ace}), FourOfAKind
+	if act != exp {
+		t.Errorf("Expected %v, got %v", exp, act)
+	}
+}
