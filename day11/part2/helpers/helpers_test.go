53aadfb27c88c3355fc2d06cd36f5d3033afe1f0
Nick Hamblet <nick.hamblet@gmail.com>
2023-12-11 22:10:01 -0800

    Repair non-compiling test code

--- day11/part2/helpers/helpers_test.go
+++ day11/part2/helpers/helpers_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func testLines() []string {
-	return []string {
+	return []string{
 		"...#......",
 		".......#..",
 		"#.........",
@@ -20,13 +20,13 @@ func testLines() []string {
 }
 
 func TestSolve(t *testing.T) {
-	var n, s int
-	n, s = Solve(testLines(), 10)
-	if n + s * 10 != 1030 {
-		t.Errorf("Expected 1030 for 10, got %v", n + s * 10)
+	var act, exp int
+	act, exp = Solve(testLines(), 10), 1030
+	if act != exp {
+		t.Errorf("Expected %v, got %v", exp, act)
 	}
-	n, s = Solve(testLines(), 100)
-	if n + s * 100 != 8410 {
-		t.Errorf("Expected 8410 for 100, got %v", n + s * 100)
+	act, exp = Solve(testLines(), 100), 8410
+	if act != exp {
+		t.Errorf("Expected %v, got %v", exp, act)
 	}
 }
